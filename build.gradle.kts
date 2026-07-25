655efd25203b48bcaf34412835903321271b9dbe
Stanislas <alexforele@gmail.com>
2026-07-25 12:44:28 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,3 @@
-// Fichier racine — ne pas mettre de code android ici
 plugins {
     id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.20" apply false
