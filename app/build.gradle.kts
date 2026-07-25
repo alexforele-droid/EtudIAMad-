bdd271a1344289536ae4d8c4a454ef15a8ef2656
Stanislas <alexforele@gmail.com>
2026-07-25 11:32:42 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,2 +1,13 @@
-namespace = "com.etudamada.etudiamad"
-applicationId = "com.etudamada.etudiamad"
+android {
+    namespace = "com.etudamada.etudiamad"
+    // ...
+
+    defaultConfig {
+        applicationId = "com.etudamada.etudiamad"
+        minSdk = 26
+        targetSdk = 34          // ou 35
+        versionCode = 1
+        versionName = "1.0"
+        // ...
+    }
+}
